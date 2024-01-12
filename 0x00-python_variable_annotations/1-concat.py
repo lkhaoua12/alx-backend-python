6573a623087a1468bc5b21765dac4a8cb83cd2ae
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-12 12:39:48 +0100

    concat two str

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module concatenates two strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This function concatenates two strings """
+    return str1 + str2
