3fbd8c18d5e4f50f9e2d1c6efc30d7fa4cd2f7f8
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-12 12:55:42 +0100

    return sum of float list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that takes a float n as argument and returns """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Function that takes a float list as argument and return sum sum_list"""
+    return sum(input_list)
