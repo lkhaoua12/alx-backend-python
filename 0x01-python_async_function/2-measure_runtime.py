863b951c5f27636a30a4ea42c2d314491e82eeee
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-15 21:12:08 +0100

    getting runtime of coroutine

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,9 +7,9 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Use the time module to measure an approximate elapsed time."""
     start = time.perf_counter()
-    await asyncio.run(wait_n(n, max_delay))
+    asyncio.run(wait_n(n, max_delay))
     end = time.perf_counter()
     return end - start / n
