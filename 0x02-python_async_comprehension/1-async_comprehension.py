95a52a51cee0bafc3174badfe6f43cd384741c05
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-16 21:40:54 +0100

    mesuarin time

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,5 +7,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """  a coroutine called async_generator that takes no arguments """
     call_list = [random async for random in async_generator()]
     return call_list
