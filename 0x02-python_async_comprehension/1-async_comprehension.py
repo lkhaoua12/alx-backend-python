bf5237b34c0654fa489a8700fa0313eacf187fdd
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-01-16 21:37:06 +0100

    using async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""  a coroutine called async_generator that takes no arguments """
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    call_list = [random async for random in async_generator()]
+    return call_list
